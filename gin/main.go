4d142061c5f8cb7a88a10d2028cc343078535459
perrychann <17397116+perrychan1@users.noreply.github.com>
2022-07-03 11:49:30 +0800

    first

---
+++ gin/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	//	r.GET("/ping", func(c *gin.Context) {
+	//		c.JSON(http.StatusOK, gin.H{
+	//			"message": "pong",
+	//		})
+	//	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "%s\n", "pong")
+	})
+
+	r.Run()
+}
