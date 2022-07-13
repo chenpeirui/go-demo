fda4617e29796db568783c6e1d9cd02099029b94
perrychann <17397116+perrychan1@users.noreply.github.com>
2022-07-13 09:07:36 +0800

    update

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func sayHello() {
+	fmt.Println("Hello")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	sayHello()
+	a := "a"
+	time.Sleep(time.Second)
+	fmt.Println("Hello, Again!")
+	fmt.Println(a)
 }
